dd8e2a96041a9bb8f524df25d7d09fd6601c4177
gwongwen <gwongwen.trash@gmail.com>
2023-09-12 11:48:28 +0200

    problem: no hello world console

--- src/main.c
+++ src/main.c
@@ -5,13 +5,11 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <zephyr/sys/printk.h>
 #include <zephyr/kernel.h>
 #include <stdio.h>
 
 int main(void)
 {
-    //printf("Hello World! %s\n", CONFIG_BOARD);
-    printk("Hello World! %s\n", CONFIG_BOARD);
+	printk("Hello World! %s\n", CONFIG_BOARD);
 	return 0;
 }
